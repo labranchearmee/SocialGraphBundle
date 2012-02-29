8c5aa1c6f93b85f4b4ed9121c9a75bb432d32db8
brickstorm <bbergstorm@gmail.com>
2012-02-29 18:24:36 +0100

    fb connect

--- DependencyInjection/GraphTwitterExtension.php
+++ DependencyInjection/GraphTwitterExtension.php
@@ -15,6 +15,46 @@ class GraphTwitterExtension extends Extension
     }
 
     /**
+     * appel api
+     *
+     * @param $message Message à envoyer à Twitter
+     * @return array
+     */
+    function requestAPI($url, $method='GET', $params) {
+      
+      $tmhOAuth = new tmhOAuth(array(
+      'consumer_key' => '',
+      'consumer_secret' => '',
+      'user_token' => '',
+      'user_secret' => '',
+      ));
+      
+      $tmhOAuth->request($method, 
+                         $tmhOAuth->url('statuses/update'), 
+                         $params
+                        );
+      
+      if ($tmhOAuth->response['code'] == 200) {
+        return $tmhOAuth->pr(json_decode($tmhOAuth->response['response'] ));
+      } else {
+        //  $tmhOAuth->pr(htmlentities($tmhOAuth->response['response']));
+        return false;
+      }
+    }
+
+    /**
+     * recherche des tweets
+     *
+     * @param $message Message à envoyer à Twitter
+     * @return array
+     */
+    function search($q, $nb_results = 100, $since_id = null) {
+      
+      $results = $this->requestAPI($tmhOAuth->url('/search.json?q='.urlencode($q).'&rpp='.$nb_results.'&since_id='.$since_id.'&include_entities=true');
+      
+    }
+
+    /**
      * Envoie un message sur twitter (stat
      * Si le message, une fois encodé en UTF-8, fait plus de 140 caractères, alors il ne sera pas accepté par Twitter.
      *
@@ -30,28 +70,9 @@ class GraphTwitterExtension extends Extension
      'user_secret' => '',
      ));
      
-     $tmhOAuth->request('POST', $tmhOAuth->url('statuses/update'), array(
-     'status' => utf8_encode($message)
-     ));
-     
-     if ($tmhOAuth->response['code'] 
-     
-     == 200) {
-     // En cours de dév, afficher les informations retournées :
-     //  $tmhOAuth->pr(json_decode($tmhOAuth->response['response'] 
-     
-     
-     
-    ));
-     return TRUE;
-     } else {
-     // En cours de dév, afficher les informations retournées :
-     //  $tmhOAuth->pr(htmlentities($tmhOAuth->response['response'] 
-     
-     
-     
-    ));
-     return FALSE;
-     }
+     $this->requestAPI($tmhOAuth->url('statuses/update'), 
+                       'POST', 
+                       array('status' => utf8_encode($message))
+                       );
     }
 }
